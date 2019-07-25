fdc23ae96923cac3b12e61ed1200b0fd6bcd6c4d
Richie <richieganney@gmail.com>
2019-07-25 16:55:24 +0100

    implemented weather API. still need to refactor

--- interface.js
+++ interface.js
@@ -33,4 +33,13 @@ $(document).ready(function() {
       $('#temperature').text(thermostat.temperature);
       $('#temperature').attr('class', thermostat.checkUsage());
     };
+
+    $('#current-city').change(function() {
+      var city = $('#current-city').val();
+      $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp)
+      })
+    })
+
 });
+
