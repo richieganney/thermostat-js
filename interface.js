da7eac71a3b41cbe3842f80eb2c83cb2a606ec90
Richie <richieganney@gmail.com>
2019-07-25 15:39:50 +0100

    switching roles

--- interface.js
+++ interface.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     var thermostat = new Thermostat();
-    $('#temperature').text(thermostat.temperature);
+    $('#temperature').text(thermostat._temperature);
     
     $('#temp-up').on('click', function() { // event listener
         thermostat.increase(1); // update model
-        $('#temperature').text(thermostat.temperature); // update view
+        $('#temperature').text(thermostat._temperature); // update view
     });
 
     $('#temp-down').on('click', function() { // event listener
         thermostat.decrease(1); // update model
-        $('#temperature').text(thermostat.temperature); // update view
+        $('#temperature').text(thermostat._temperature); // update view
     });
   });
 
